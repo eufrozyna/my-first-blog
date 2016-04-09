3d459843a2e6e05a64736e7b0b1418d6958199af
eufrozyna <marbook@wp.pl>
2016-04-09 17:51:06 +0200

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,24 @@
+# #if imie == 'Ola':
+#     print('Hej Ola!')
+# elif imie == 'Marta':
+#     print('Hej Marta!')
+# else:
+#     print('Hej nieznajoma!')
+dziewczyny = ["Kasia", "Kamila", "Agnieszka", "Agata", "Julia"]
+def hej (imie):
+    #print("Hej!")
+    #print("Jak się masz?")
+    # if imie == 'Ola':
+    #     print('Hej Ola!')
+    # elif imie == 'Marta':
+    #     print('Hej Marta!')
+    # else:
+    #     print('Hej nieznajoma!')
+    print("Hej " + imie + "!")
+
+# for imie in dziewczyny: 
+#     hej(imie)
+#     print("Kolejna dziewczyna")
+
+for i in range(1,6):
+    print(i)
